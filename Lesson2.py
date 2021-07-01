f3a189ca585438f4a12628ce760fc116d69cdf62
Zo1e <86787834+Zo1e@users.noreply.github.com>
2021-07-01 18:32:03 +0400

    Delete Lesson2.py

--- Lesson2.py
+++
@@ -1,9 +0,0 @@
-time = int(input('Введите время в секундах -'))
-minutes = int(time / 60)
-hours = int(minutes / 60)
-seconds = (time % 60)
-minutes1 = (minutes % 60)
-print(f'Время - {hours} : {minutes1} : {seconds}')
-
-
-
